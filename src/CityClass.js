142e21e95390eba32ced1c8fe5a2bd32f7ac51a1
RadonNazar <radon.bogdan09@gmail.com>
2025-06-24 16:02:44 +0300

    Add files via upload

---
+++ src/CityClass.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CityClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Рівне</h1>
+        <p>Країна: Україна</p>
+        <p>Рік заснування: 1283</p>
+        <h2>Пам'ятки</h2>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Rivne_drama_theatre.jpg" alt="Драмтеатр Рівне" width="250" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Rivne_Zhyttya.jpg" alt="Центр міста" width="250" />
+        <h2>Опис</h2>
+        <p>Рівне - обласний центр Рівненської області, розташований у північно-західній частині України.</p>
+      </div>
+    );
+  }
+}
+
+export default CityClass;
---
+++ src/CityFunctional.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function CityFunctional() {
+  return (
+    <div>
+      <h1>Рівне</h1>
+      <p>Країна: Україна</p>
+      <p>Рік заснування: 1283</p>
+      <h2>Пам'ятки</h2>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Rivne_drama_theatre.jpg" alt="Драмтеатр Рівне" width="250" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Rivne_Zhyttya.jpg" alt="Центр міста" width="250" />
+      <h2>Опис</h2>
+      <p>Рівне - обласний центр Рівненської області, розташований у північно-західній частині України.</p>
+    </div>
+  );
+}
+
+export default CityFunctional;
